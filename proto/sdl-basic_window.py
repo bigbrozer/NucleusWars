2b2c83c466a969b6821540f5249ca225da50d25e
Vincent BESANCON <besancon.vincent@gmail.com>
2014-06-05 00:22:34 +0200

    [proto] show a basic window using PySDL.

---
+++ proto/sdl-basic_window.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# NucleusWars
+# Copyright (C) 2014 Vincent Besançon
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""Show a basic window using PySDL."""
+
+import sys
+import sdl2
+import sdl2.ext
+
+
+def run():
+    sdl2.ext.init()
+    window = sdl2.ext.Window("Basic Window", size=(800, 600))
+    sdl2.ext.fill(window.get_surface(), sdl2.ext.Color(0, 0, 0))
+    window.show()
+    running = True
+    while running:
+        events = sdl2.ext.get_events()
+        for event in events:
+            if event.type == sdl2.SDL_QUIT:
+                running = False
+                break
+        window.refresh()
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(run())
